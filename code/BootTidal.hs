5c357eee7496d348803076e19b476245b4dcc4d7
jswirbs <swirbulj@gmail.com>
2026-07-05 23:07:09 -0400

    updates alllllll around town

--- code/BootTidal.hs
+++ code/BootTidal.hs
@@ -1,98 +1,65 @@
-:set -XOverloadedStrings
+:set -fno-warn-orphans -Wno-type-defaults -XMultiParamTypeClasses -XOverloadedStrings
 :set prompt ""
 
-import Sound.Tidal.Context
-import System.IO (hSetEncoding, stdout, utf8)
-hSetEncoding stdout utf8
+-- Import all the boot functions and aliases.
+import Sound.Tidal.Boot
 
--- for custom resetCycles impl
--- import Sound.Tidal.Tempo
-import qualified Sound.Tidal.Tempo as T
+default (Rational, Integer, Double, Pattern String)
 
--- total latency = oLatency + cFrameTimespan
+-- Create a Tidal Stream with the default settings.
+-- To customize these settings, use 'mkTidalWith' instead
+tidalInst <- mkTidal
 
--- # START TIDAL NORMALLY (just SuperCollider)
--- tidal <- startTidal (superdirtTarget {oLatency = 0.1, oAddress = "127.0.0.1", oPort = 57120}) (defaultConfig {cFrameTimespan = 1/20})
+-- tidalInst <- mkTidalWith [(superdirtTarget { oLatency = 0.01 }, [superdirtShape])] (defaultConfig {cFrameTimespan = 1/50, cProcessAhead = 1/20})
 
 -- # START TIDAL CONNECTED TO SuperCollider AND Processing (removing above and adding processing target)
-processingTarget = Target { oName = "processing", oAddress = "127.0.0.1", oPort = 3333, oBusPort = Nothing, oLatency = 0.01, oWindow = Nothing, oSchedule = Live, oHandshake = False }
-tidal <- startStream (defaultConfig {cFrameTimespan = 1/20}) [(superdirtTarget, [superdirtShape]), (processingTarget, [superdirtShape])]
-
-:{
-let p = streamReplace tidal
-    -- hush = streamHush tidal
-    list = streamList tidal
-    mute = streamMute tidal
-    unmute = streamUnmute tidal
-    solo = streamSolo tidal
-    unsolo = streamUnsolo tidal
-    once = streamOnce tidal
-    first = streamFirst tidal
-    asap = once
-    nudgeAll = streamNudgeAll tidal
-    all = streamAll tidal
-    -- resetCycles = streamResetCycles tidal
-    resetCycles = T.changeTempo (sTempoMV tidal) (\t tempo -> tempo {T.atTime = t + 0.1, T.atCycle = 0})
-    setcps = asap . cps
-    xfade i = transition tidal True (Sound.Tidal.Transition.xfadeIn 4) i
-    xfadeIn i t = transition tidal True (Sound.Tidal.Transition.xfadeIn t) i
-    histpan i t = transition tidal True (Sound.Tidal.Transition.histpan t) i
-    wait i t = transition tidal True (Sound.Tidal.Transition.wait t) i
-    waitT i f t = transition tidal True (Sound.Tidal.Transition.waitT f t) i
-    jump i = transition tidal True (Sound.Tidal.Transition.jump) i
-    jumpIn i t = transition tidal True (Sound.Tidal.Transition.jumpIn t) i
-    jumpIn' i t = transition tidal True (Sound.Tidal.Transition.jumpIn' t) i
-    jumpMod i t = transition tidal True (Sound.Tidal.Transition.jumpMod t) i
-    mortal i lifespan release = transition tidal True (Sound.Tidal.Transition.mortal lifespan release) i
-    interpolate i = transition tidal True (Sound.Tidal.Transition.interpolate) i
-    interpolateIn i t = transition tidal True (Sound.Tidal.Transition.interpolateIn t) i
-    clutch i = transition tidal True (Sound.Tidal.Transition.clutch) i
-    clutchIn i t = transition tidal True (Sound.Tidal.Transition.clutchIn t) i
-    anticipate i = transition tidal True (Sound.Tidal.Transition.anticipate) i
-    anticipateIn i t = transition tidal True (Sound.Tidal.Transition.anticipateIn t) i
-    forId i t = transition tidal False (Sound.Tidal.Transition.mortalOverlay t) i
-    d1 = p 1 . (|< orbit 0)
-    d2 = p 2 . (|< orbit 1)
-    d3 = p 3 . (|< orbit 2)
-    d4 = p 4 . (|< orbit 3)
-    d5 = p 5 . (|< orbit 4)
-    d6 = p 6 . (|< orbit 5)
-    d7 = p 7 . (|< orbit 6)
-    d8 = p 8 . (|< orbit 7)
-    d9 = p 9 . (|< orbit 8)
-    d10 = p 10 . (|< orbit 9)
-    d11 = p 11 . (|< orbit 10)
-    d12 = p 12 . (|< orbit 11)
-    d13 = p 13
-    d14 = p 14
-    d15 = p 15
-    d16 = p 16
-    sidechain = pI "sidechain"
-    sidechain_thresh = pF "sidechain_thresh"
-    sidechain_compression = pF "sidechain_compression"
-    sidechain_attack = pF "sidechain_attack"
-    sidechain_release = pF "sidechain_release"
-:}
-
-:{
-let setI = streamSetI tidal
-    setF = streamSetF tidal
-    setS = streamSetS tidal
-    setR = streamSetR tidal
-    setB = streamSetB tidal
-:}
+-- # !not tested with new installation...
+-- processingTarget = Target { oName = "processing", oAddress = "127.0.0.1", oPort = 3333, oBusPort = Nothing, oLatency = 0.01, oWindow = Nothing, oSchedule = Live, oHandshake = False }
+-- tidal <- startStream (defaultConfig {cFrameTimespan = 1/20}) [(superdirtTarget, [superdirtShape]), (processingTarget, [superdirtShape])]
 
 
----- CUSTOM SETUP ----
+-- This orphan instance makes the boot aliases work!
+-- It has to go after you define 'tidalInst'.
+instance Tidally where tidal = tidalInst
 
+-- `enableLink` and `disableLink` can be used to toggle synchronisation using the Link protocol.
+-- Uncomment the next line to enable Link on startup.
+-- enableLink
+
+
+-- CLOCK
 -- sends tick pattern to supercollider which will then display basic clock information
 let hush = do streamHush tidal; p "tick" $ "0*4" # s "tick"
 p "tick" $ "0*4" # s "tick"
 
+
+-- ALIASES
+
 bpm b = setcps (b/4/60)
 
-----------------------
+sidechain = pI "sidechain"
+sidechain_thresh = pF "sidechain_thresh"
+sidechain_compression = pF "sidechain_compression"
+sidechain_attack = pF "sidechain_attack"
+sidechain_release = pF "sidechain_release"
 
+adsr a d s r = attack a # decay d # sustain s # release r
+rvb r s      = room r # size s
+tremolo r d  = tremolorate r # tremolodepth d
+phaser  r d  = phaserrate r # phaserdepth d
+
+sc o t       = sidechain o # sidechain_thresh (t/100) # sidechain_compression ("10"/100) # sidechain_attack ("1"/1000) # sidechain_release ("20"/100)
+
+-- fibonacci sequence, gives nth term
+:{
+fib:: (Eq a, Num a, Num p) => a -> p
+fib 0 = 0
+fib 1 = 1
+fib n = fib (n-1)+ fib (n-2)
+:}
+
+
+--
 
 :set prompt "tidal> "
 :set prompt-cont ""
